85b026f4ee3ff36874e4bc66c7cc12d516394b77
Keerthana N <kk1106726@gmail.com>
2026-01-21 09:53:02 +0530

    Create App component for CA Monk Blog App

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>CA Monk Blog App</h1>
+      <p>If you see this text, App.tsx is working.</p>
+    </div>
+  );
+}
 
